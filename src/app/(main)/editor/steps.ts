7313ab445dfa1cfb67159549bea1f64536f6e16f
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2024-11-30 04:23:41 +0530

    education form (day-3)

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,6 +1,8 @@
 import { EditorFormProps } from "@/lib/types";
 import  GeneralInfoForm  from "./forms/GeneralInfoForm";
 import  PersonalInfoForm  from "./forms/PersonalInfoForm";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
+import EducationForm from "./forms/EducationForm";
 
 
 export const steps: {
@@ -11,5 +13,8 @@ export const steps: {
 }[] = [
     {title:"General Info", component: GeneralInfoForm , key:"general-info"},
     {title:"Personal Info", component: PersonalInfoForm , key:"personal-info"},
+    {title:"Work experience", component: WorkExperienceForm , key:"work-experience"},
+    {title:"Education", component: EducationForm , key:"education"},
+
 
 ]
