6c4408968984abe9169cd4437d64cd10c9dbfd26
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2025-01-21 18:05:03 +0530

    fixed unnecessary migrations

--- prisma/migrations/20241226131240_add_description_to_models/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "custom_codingProfiles" ADD COLUMN     "description" TEXT;
-
--- AlterTable
-ALTER TABLE "custom_contests" ADD COLUMN     "description" TEXT;
-
--- AlterTable
-ALTER TABLE "custom_courses" ADD COLUMN     "description" TEXT;
